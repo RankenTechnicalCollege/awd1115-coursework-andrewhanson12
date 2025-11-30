03944c5046e576b64ca17db492bcaf4c895ed8ed
andrewhanson12 <andrew_hanson@insideranken.org>
2025-11-30 16:27:07 -0600

    Commit CH16 Lab

---
+++ Labs/CH16/StripeWebApp/StripeWebApp/Models/Data/MvcContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StripeWebApp.Models.Data
+{
+    public class MvcContext : DbContext
+    {
+        public MvcContext(DbContextOptions<MvcContext> options) : base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ Labs/CH16/StripeWebApp/StripeWebApp/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Stripe;
+using StripeWebApp.Models.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Payment}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
